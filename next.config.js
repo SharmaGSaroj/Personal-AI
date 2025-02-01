25094270ac5fe1a0f1ac12f05198f4d65ac3e38e
SharmaGSaroj <91160959+SharmaGSaroj@users.noreply.github.com>
2025-02-01 00:37:14 -0500

    ai-problem

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 module.exports = {
-    reactStrictMode: true,
-  }
-  
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://127.0.0.1:3001/api/:path*', // Proxy to Backend
+      },
+    ]
+  },
+}
